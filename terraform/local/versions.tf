874df6f270320342ecff0afbc9742769450ce54e
sohelsaiyed <sohel.saiyed@alfresco.com>
2025-03-07 16:47:07 +0000

    fix versio formatting

--- terraform/local/versions.tf
+++ terraform/local/versions.tf
@@ -1,14 +1,17 @@
 terraform {
   required_version = ">= 1.0"  # Adjust the version according to your needs
+  
   required_providers {
     external = {
       source  = "hashicorp/external"
       version = ">= 2.3.0"
     }
+    
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.22.0"
     }
+    
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.11.0"
